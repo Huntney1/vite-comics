cd35cc2e94fcfc5c3995b2e392e6fc2826ab65cb
Huntney1 <devilwhiteone@hotmail.it>
2023-01-11 17:45:55 +0100

    inizio_preparazione effettuata

---
+++ vite-comics/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+/* import AppContent from "./components/AppContent.vue";
+import AppFooter from "./components/AppFooter.vue"; */
+
+export default {
+  components: {
+    AppHeader,
+  /*   AppContent,
+    AppFooter, */
+  },
+};
+</script>
+
+<template>
+  <div>
+    <AppHeader />
+    <!-- <AppContent />
+    <AppFooter /> -->
+  </div>
+</template>
+
+
+<style lang="scss">
+
+
+</style>
---
+++ vite-comics/src/components/AppHeader.vue
@@ -0,0 +1,80 @@
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+        /* lista voci nav-bar */
+      menu: [
+        {
+          label: "CHARACTERS",
+          url: "#",
+          active: false,
+        },
+        {
+          label: "COMICS",
+          url: "#",
+          active: true,
+        },
+        {
+          label: "MOVIES",
+          url: "#",
+          active: false,
+        },
+        {
+          label: "TV",
+          url: "#",
+          active: false,
+        },
+        {
+          label: "GAMES",
+          url: "#",
+          active: false,
+        },
+        {
+          label: "COLLECTIBLES",
+          url: "#",
+          active: false,
+        },
+        {
+          label: "VIDEOS",
+          url: "#",
+          active: false,
+        },
+        {
+          label: "FANS",
+          url: "#",
+          active: false,
+        },
+        {
+          label: "NEWS",
+          url: "#",
+          active: false,
+        },
+        {
+          label: "SHOP",
+          url: "#",
+          active: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <!-- logo dc -->
+    <a href="/" class="logo">
+      <img src="../assets/vue-dc-comics-1/img/dc-logo.png" alt="logo_dc" />
+    </a>
+    <!-- attivo lista nav bar -->
+    <nav>
+      <ul>
+        <li v-for="(item, index) in menu" :key="index">
+          <a :href="item.url" class="item.active ? 'active' : ''"></a>
+          {{ item.label }}
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
