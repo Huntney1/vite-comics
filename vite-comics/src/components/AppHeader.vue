b192cc10da16312a69c58b9a198965fccda56468
Huntney1 <devilwhiteone@hotmail.it>
2023-01-11 19:48:47 +0100

    header

--- vite-comics/src/components/AppHeader.vue
+++ vite-comics/src/components/AppHeader.vue
@@ -3,7 +3,7 @@ export default {
   name: "AppHeader",
   data() {
     return {
-        /* lista voci nav-bar */
+      /* lista voci nav-bar */
       menu: [
         {
           label: "CHARACTERS",
@@ -63,18 +63,53 @@ export default {
 
 <template>
   <header>
-    <!-- logo dc -->
-    <a href="/" class="logo">
-      <img src="../assets/vue-dc-comics-1/img/dc-logo.png" alt="logo_dc" />
-    </a>
-    <!-- attivo lista nav bar -->
-    <nav>
-      <ul>
-        <li v-for="(item, index) in menu" :key="index">
-          <a :href="item.url" class="item.active ? 'active' : ''"></a>
-          {{ item.label }}
-        </li>
-      </ul>
-    </nav>
+    <div class="continer">
+      <!-- logo dc -->
+      <a href="/" class="logo">
+        <img src="../assets/vue-dc-comics-1/img/dc-logo.png" alt="logo_dc" />
+      </a>
+      <!-- attivo lista nav bar -->
+      <nav>
+        <ul>
+          <li v-for="(item, index) in menu" :key="index">
+            <a :href="item.url" class="item.active ? 'active' : ''"></a>
+            {{ item.label }}
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
+
+<style lang="scss">
+@use "../styles/partials/mixins" as *;
+@use "../styles/partials/variables" as *;
+
+header {
+  /* margin-left: 10em; */
+
+ img {
+    width: 50px;
+    margin-top: 14px;
+ }
+
+ div {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+ }
+ 
+  ul {
+    @include display-flex-sb;
+   
+    margin: 25px 10px;
+    
+    list-style: none;
+  }
+
+  li {
+    margin: 6px;
+    line-height: 1px;
+  }
+}
+</style>
